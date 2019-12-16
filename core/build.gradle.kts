8b1ce5a123392488b6c37d3345a52b352df6bc72
Geoffrey Challen <challen@illinois.edu>
2019-12-16 10:51:53 -0600

    Upgrade Java and dependencies.

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,17 +1,19 @@
 plugins {
     kotlin("jvm")
     java
-    id("org.jetbrains.dokka") version "0.9.18"
+    id("org.jetbrains.dokka") version "0.10.0"
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.javassist:javassist:3.25.0-GA")
-    implementation("org.apache.bcel:bcel:6.0")
-    implementation("org.junit.jupiter:junit-jupiter:5.4.2")
-    implementation("org.objenesis:objenesis:3.0.1")
-    implementation("com.google.code.gson:gson:2.8.5")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.4.2")
+    val jupiterVersion = "5.5.2"
+    implementation(kotlin("stdlib"))
+    implementation("org.javassist:javassist:3.26.0-GA")
+    implementation("org.apache.bcel:bcel:6.4.1")
+    implementation("org.junit.jupiter:junit-jupiter:$jupiterVersion")
+    implementation("org.objenesis:objenesis:3.1")
+    implementation("com.google.code.gson:gson:2.8.6")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:$jupiterVersion")
 }
 
 tasks.dokka {
